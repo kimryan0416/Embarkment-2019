730a947ef7b3272b14ca1d8faa0cbd9adc9f3388
rk494 <rk494@cornell.edu>
2019-04-23 03:22:07 +0900

    Allowed for enemy collision with golden leaves, destroyes both golden leaf and enemy unit + camera moves with player

--- Test/Assets/Scripts/GoldenLeaf.cs
+++ Test/Assets/Scripts/GoldenLeaf.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GoldenLeaf : MonoBehaviour
 {
+    float timer = 2f; // Default time until destruction of current Golden Leaf, in case it doesn't hit anything
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +15,21 @@ public class GoldenLeaf : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        /*
         GetComponent<EdgeCollider2D>().enabled = false;
         GetComponent<EdgeCollider2D>().enabled = true;
+        */
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
         Debug.Log("I Collided With: " + collision.gameObject.name);
         /*
         if (collision.gameObject.tag == "GoldenLeaf")
